786dc11d534176c02f076b8534a220d30abee8df
stevenwalton <walton.stevenj@gmail.com>
2018-10-01 19:40:45 -0700

    Done up to 7

--- a1/trees.ml
+++ a1/trees.ml
@@ -39,26 +39,32 @@ let rec prod1 tree =
         Empty -> 1
     |   Node(i,j,k) -> i * (prod1 j * prod1 k)
 ;;
-let rec length tree =
-    match tree with
-        Empty -> 0
-    |   Node(i,j,k) -> 1 + (length j + length k)
-;;
-let rec avg1 tree =
-    (sum1 tree) / (length tree)
-;;
 
-(* Q3 *)
-(*
-let rec map =
+let avg1 tree =
+    let rec helper tree =
+        match tree with
+            Empty -> (0,0)
+        |   Node(i,l,r) ->
+                let (leftSum,leftLen) = helper l in
+                let (rightSum,rightLen) = helper r in
+                (i + leftSum + rightSum , 1 + leftLen + rightLen) in
+    let (s,len) = helper tree in
+        try s/len
+        with Division_by_zero -> 0
 ;;
-*)
+        
+(* Q3 *)
+let rec map f tree =
+    match tree with
+        Empty -> Empty
+    |   Node(i,l,r) -> 
+            Node(f i, map f l, map f r)
+;;
 
 (* Q4 *)
-(*
-let rec negateAll =
+let negateAll tree=
+    map (fun x -> x * -1) tree
 ;;
-*)
 
 let rec fold f a t =
   match t with
@@ -67,18 +73,16 @@ let rec fold f a t =
 
 (* put sum2, prod2, and avg2 here *)
 (* Q4 *)
-(*
-let rec sum2 tree =
+let sum2 tree =
+    fold (fun a j -> a + j) 0 tree
 ;;
-*)
-(*
-let rec prod2 tree =
+let prod2 tree =
+    fold (fun a j -> a * j) 1 tree
 ;;
-*)
-(*
-let rec avg2 tree =
+let avg2 tree =
+    let (s,l) = (fold (fun a j -> a + j) 0 tree, fold (fun a j -> a + 1) 0 tree) in
+    s/l
 ;;
-*)
 
 
 type 'a iterator = Nomore | More of 'a * (unit -> 'a iterator)
@@ -108,7 +112,6 @@ let rec avg3 tree =
 
 (* a little testing -- commented out since the functions do not exist yet *)
 
-(*
 let tr = fromList [0;1;2;3;4;5;6;7;8;9;9;9;1] (* repeats get removed *)
 let print_ans f t = print_string (string_of_int (f t)); print_string "\n"
 let _ = print_ans sum1 tr
@@ -117,6 +120,7 @@ let _ = print_ans avg1 tr
 let _ = print_ans sum2 tr
 let _ = print_ans prod2 tr
 let _ = print_ans avg2 tr
+(*
 let _ = print_ans sum3 tr
 let _ = print_ans prod3 tr
 let _ = print_ans avg3 tr
