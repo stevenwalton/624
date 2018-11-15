f5cfe64df1d1670f4dcfb629e07f7421a56a7be2
Steven Walton <swalton2@uoregon.edu>
2018-11-14 22:11:41 -0800

    Done

--- a6/cps.ml
+++ a6/cps.ml
@@ -1,6 +1,18 @@
 (* CPS Homework: A6 *)
+let problem a =
+    if a > 1 then print_newline();
+    print_string "===========";
+    print_newline();
+    print_string " Problem ";
+    print_int a;
+    print_newline();
+    print_string "===========";
+    print_newline();
+;;
 
-(* Problem 1: Low-level functions in CPS *)
+(* Problem 1: Low-level (trivial) functions in CPS *)
+problem 1;;
+
 let rec addk (a:int) (b:int) k =
     k (a + b)
 ;;
@@ -30,8 +42,13 @@ let catk (a:string) (b:string) k =
 ;;
 
 let consk a lst k =
+    k (a::lst)
+;;
+(*
+let consk a lst k =
     k (lst @ [a])
 ;;
+*)
 
 let lessk a b k =
     k (a < b)
@@ -49,3 +66,96 @@ let report x =
 addk 3 4 report;;
 
 (* Problem 2: Nesting continuations *)
+(* you have a typo in the instructions here *)
+problem 2;;
+
+let add3k a b c k =
+    addk a b (fun ab -> addk ab c k)
+;;
+
+add3k 1 2 3 report;;
+
+(* a * (b + c) *)
+let mult3k a b c k =
+    multk a b (fun ab -> plusk ab c k)
+;;
+
+mult3k 1. 2. 3. (fun y -> report (int_of_float y));;
+
+(* (a + b) * c *)
+let multf3k a b c k =
+    plusk a b (fun ab -> multk ab c k)
+;;
+
+multf3k 1. 2. 3. (fun y -> report (int_of_float y));;
+
+let abcdk a b c d k =
+    multk b c (fun bc -> 
+        plusk a bc (fun abc -> 
+            multk d a (fun da ->
+                plusk abc da k
+            )
+        )
+    )
+;;
+abcdk 2.0 3.0 4.0 5.0 (fun y -> report (int_of_float y));;
+
+(* Problem 3: Recursion :*)
+problem 3;;
+
+let rec factorial n = 
+    if n = 0
+    then 1
+    else n * factorial (n-1)
+;;
+
+let rec factoriale n =
+    let b = n = 0 in
+        if b then 1
+        else let s = n - 1 in
+            let m = factoriale s in 
+                n * m
+;;
+
+
+let rec fact_range n m =
+    if n < m then 1
+    else if n = 0 then 1
+    else n * fact_range (n-1) m 
+;;
+
+let rec fact_rangek n m k =
+    if n < m
+    then k 1
+    else fact_rangek (n-1) m (fun z -> k (n * z))
+;;
+
+fact_rangek 7 5 report;;
+
+(* Problem 4: List recursion *)
+problem 4;;
+
+let rec app_all flst x =
+    match flst with
+      [] -> x
+    | h::t -> app_all t (h x)
+;;
+
+(* returns 36 because does x + 1 then x*x *)
+let rec app_all flst x =
+    match flst with
+      [] -> []
+    | h::t -> h x :: app_all t x
+;;
+    (*| h::t -> let z = h x in app_all t z*)
+
+app_all [((+) 1); (fun x -> x*x); (fun x -> 13)] 5;;
+
+let rec app_allk flstk x k =
+    match flstk with
+      [] -> []
+    | h::t -> h x k :: app_allk t x k
+;;
+
+app_allk [(addk 1); (fun x -> timesk x x); (fun x -> (fun k -> k 13))] 5 (fun x -> x);;
+
